853760786acffa28855d7d1d318840dd5f1c85b7
MutantMist <MutantMist@Outlook.com>
2023-12-02 07:41:09 +0530

    V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,25 @@
-;(function($) {
-
-    $(document).ready( function() {
+(function($) {
+    $(document).ready(function() {
+        // Toggle menu visibility
         $(document).on('click', '.header-area .show-menu', function() {
             $(this).toggleClass('active');
             $(".header-area .navbar").toggleClass('active');
         });
-        // $(document).on('click', '.header-area .navbar .close-menu', function() {
-        //     $(".header-area .navbar").removeClass('active');
-        // });
 
+        // Initialize AOS (Animate On Scroll)
         AOS.init({
             duration: 1500,
             once: true,
-        })
+        });
+
+        // Preloader logic
+        var div = document.createElement("div");
+        div.id = "preloader";
+        div.className = "preloader";
+        div.innerHTML = '<div class="black_wall"></div><div class="loader"></div>';
+        document.body.insertBefore(div, document.body.firstChild);
+        window.onload = function() {
+            document.getElementById("preloader").classList.add("off");
+        };
     });
-
 })(jQuery);
-
-
-var div = document.createElement("div");
-    div.id="preloader",
-    div.className="preloader",
-    div.innerHTML='<div class="black_wall"></div><div class="loader"></div>',
-    document.body.insertBefore(div,document.body.firstChild),window.onload=function() {
-    document.getElementById("preloader").classList.add("off")
-};
